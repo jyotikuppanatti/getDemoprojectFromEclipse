f54841db1d80e62805a4c2958d3ca86566dda1b8
jyotikuppanatti <jyotikuppanatti@gmail.com>
2018-07-12 02:04:01 +0530

    bhjg

---
+++ demo/src/main/java/packagrname/demo/App.java
@@ -0,0 +1,13 @@
+package packagrname.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
